01f3bf17e8b1abfebcdc6ddfb8b2c70d4ca81683
kud2207 <ulrichkageu@gamil.com>
2025-04-30 18:18:26 +0100

    apk1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,6 +12,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
@@ -26,7 +27,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.4.2'
-        classpath 'com.google.gms:google-services:4.3.15' // ✅ ajoute cette ligne
+        classpath("com.android.tools.build:gradle:7.4.2")  // Utilise des guillemets doubles
+        classpath("com.google.gms:google-services:4.3.15") // Utilise des guillemets doubles
     }
 }
